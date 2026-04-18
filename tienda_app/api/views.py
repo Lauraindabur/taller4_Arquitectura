41c6080f8bd9b625555992a842aac25bfa528f46
Lauraindabur <lindaburg@eafit.edu.co>
2026-04-18 13:46:36 -0500

    Añadir endpoints para taller 6

--- tienda_app/api/views.py
+++ tienda_app/api/views.py
@@ -3,9 +3,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from tienda_app.infra.factories import PaymentFactory
+from tienda_app.models import Libro
 from tienda_app.services import CompraService
 
-from .serializers import OrdenInputSerializer
+from .serializers import LibroSerializer, OrdenInputSerializer
+
+
+class ProductosAPIView(APIView):
+    """
+    Endpoint de lectura para coexistencia v1.
+    GET /api/v1/productos/
+    """
+
+    def get(self, request):
+        productos = Libro.objects.all().order_by('id')
+        serializer = LibroSerializer(productos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class CompraAPIView(APIView):
